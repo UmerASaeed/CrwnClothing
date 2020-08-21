126abaa14567be7ac2907c7d8bde886c7bd532ea
UmerASaeed <53197650+UmerASaeed@users.noreply.github.com>
2020-08-21 21:23:19 +0500

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom'
+
+import {connect} from "react-redux"
+import {Route, Switch ,Redirect} from 'react-router-dom'
 import HomePage from './pages/homePage/homepage.component'
 import ShopPage from './pages/shop/shopPage.component'
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 import {auth,CreateUserProfileDocument} from './firebase/firebase.utils'
+import {setCurrentUser} from './redux/user/user.action'
 
 class App extends React.Component 
 {
-  constructor()
-  {
-    super();
-    this.state={
-      currentUser:null
-    }  
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount()
   {
+    const {setCurrentUser} =this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>
       {
         if (userAuth)
@@ -29,16 +25,14 @@ class App extends React.Component
          
           (await userRef).onSnapshot(snapShot=>
             {
-             this.setState({
-               currentUser: {
+             setCurrentUser ({
                 id:snapShot.id,
                 ...snapShot.data()
-               }
-             })
+              })
             })
         }
         else{
-          this.setState({currentUser:userAuth})
+          setCurrentUser(userAuth)
         }
       })
   }
@@ -51,15 +45,28 @@ class App extends React.Component
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route exact path='/shop' component={ShopPage}/>
-            <Route exact path='/signin' component={SignInAndSignUpPage}/>
+            <Route exact path='/signin' render = {() => this.props.currentUser ? <Redirect to="/"/> : <SignInAndSignUpPage/>}/>
         </Switch>    
       </div>
   );
   }
 }
 
-export default App;
+const mapStateToProps = state =>
+({
+  currentUser:state.user.currentUser
+})
+
+const mapDispatchToProps = dispatch =>(
+{
+ setCurrentUser : user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
+
+
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/form-input.component'
 import './sign-in.styles.scss'
 import '../custom-button/custom-button.component'
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils'
 
 class Signin extends React.Component
 {
@@ -16,10 +16,22 @@ class Signin extends React.Component
         }
     }
 
-    handleSubmit=(event)=>
+    handleSubmit=async (event)=>
     {
         event.preventDefault();
-        this.setState({email:"",password:""})
+        const {email,password}=this.state;
+        
+        try
+        {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:"",password:""})
+        }
+
+        catch(err)
+        {
+            console.log(err)
+        }
+
     }
 
     handleChange=(event)=>
@@ -40,7 +52,7 @@ class Signin extends React.Component
                     <FormInput name="password" type='password' value={this.state.password} label='Password' handleChange={this.handleChange} required/>
                     <div className='buttons'>
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} googleSignIn='googleSignIn'> Sign in with Google </CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} googleSignIn='googleSignIn'> Sign in with Google </CustomButton>
                     </div>
                 </form>
             </div>
